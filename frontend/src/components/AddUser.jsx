48c79421f6c7ac4e747a1b5a8d9c128363e7ed18
Saqif <saqifhaque@gmail.com>
2023-01-11 20:52:30 +0600

    Add User component form create and input binding

--- frontend/src/components/AddUser.jsx
+++ frontend/src/components/AddUser.jsx
@@ -1,10 +1,59 @@
+import { useState } from 'react';
+import { FormControl, FormGroup, InputLabel, Input, Typography, styled, Button } from "@mui/material";
+
+import { addUser } from "../service/api";
+
+const Container = styled(FormGroup)`
+    width:50%;
+    margin: 5% auto 0 auto;
+    & > div {
+        margin-top:20px;
+    }
+`
+
+const defaultValue = {
+    name: '',
+    username: '',
+    email: '',
+    phone: ''
+}
 
 
 
 const AddUser = () => {
+    const [user, setUser] = useState(defaultValue);
+
+    const onValueChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value})
+    }
+
+    const addUserDetails = async () => {
+        await addUser(user);
+    }
 
     return (
-        <p>Hello world</p>
+        <Container>
+            <Typography variant="h4">Add User</Typography>
+            <FormControl>
+                <InputLabel> Name </InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name="name"></Input>
+            </FormControl>
+            <FormControl>
+                <InputLabel> Username </InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name="username"></Input>
+            </FormControl>
+            <FormControl>
+                <InputLabel> Email </InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name="email"></Input>
+            </FormControl>
+            <FormControl>
+                <InputLabel> Phone </InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name="phone"></Input>
+            </FormControl>
+            <FormControl>
+                <Button onClick={() => addUserDetails()} variant="contained">Add User</Button>
+            </FormControl>
+        </Container>
     )
 }
 
---
+++ frontend/src/service/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const URL = '';
+
+export const addUser = async (data) => {
+    try {
+       return await axios.post(`${URL}/add`, data)
+    } catch(error) {
+        console.log("Error while calling Api", error);
+    }
+}
