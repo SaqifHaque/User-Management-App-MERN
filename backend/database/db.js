f48a27baf10d168f1990e91f688d5dfe1817a3fb
Saqif <saqifhaque@gmail.com>
2023-01-12 16:50:30 +0600

    connecting mongodb atlas with server using mongoose

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
 
-const connection = () => {
+const connection = async (user, pass) => {
+    const url= `mongodb+srv://${user}:${pass}@mern-crud-app.2mxk5re.mongodb.net/?retryWrites=true&w=majority`;
     try {
-
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log("MongoDB connected successfully");
     } catch(error) {
         console.log('Error while connecting with database', error);
     }
 }
+
+export default connection; 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import Connection from './database/db.js';
 
 const app = express();
 
-const port = 8000;
+dotenv.config();
+
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const port = process.env.PORT;
+
+Connection(dbUser, dbPass);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
